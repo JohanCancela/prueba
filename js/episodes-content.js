58372cbf91f85e53c48412276a6374630600a8d1
JohanCancela <105380656+JohanCancela@users.noreply.github.com>
2022-06-14 16:16:14 -0500

    Add files via upload

--- js/episodes-content.js
+++ js/episodes-content.js
@@ -1,12 +1,54 @@
 const watch = document.querySelector('.botton-watch');
 const uno = document.querySelector('.uno');
 const dos = document.querySelector('.dos');
+const tres = document.querySelector('.tres');
+const cuatro = document.querySelector('.cuatro');
+const cinco = document.querySelector('.cinco');
+const seis = document.querySelector('.seis');
+const siete = document.querySelector('.siete');
+const ocho = document.querySelector('.ocho');
+const nueve = document.querySelector('.nueve');
+const diez = document.querySelector('.diez');
+const once = document.querySelector('.once');
+const doce = document.querySelector('.doce');
+const trece = document.querySelector('.trece');
+const catorce = document.querySelector('.catorce');
+const quince = document.querySelector('.quince');
+const dieciseis = document.querySelector('.dieciseis');
 const trailer = document.querySelector('.trailer');
 const episodeUno = document.querySelector('.episode-uno');
 const episodeDos = document.querySelector('.episode-dos');
+const episodeTres = document.querySelector('.episode-tres');
+const episodeCuatro = document.querySelector('.episode-cuatro');
+const episodeCinco = document.querySelector('.episode-cinco');
+const episodeSeis = document.querySelector('.episode-seis');
+const episodeSiete = document.querySelector('.episode-siete');
+const episodeOcho = document.querySelector('.episode-ocho');
+const episodeNueve = document.querySelector('.episode-nueve');
+const episodeDiez = document.querySelector('.episode-diez');
+const episodeOnce = document.querySelector('.episode-once');
+const episodeDoce = document.querySelector('.episode-doce');
+const episodeTrece = document.querySelector('.episode-trece');
+const episodeCatorce = document.querySelector('.episode-catorce');
+const episodeQuince = document.querySelector('.episode-quince');
+const episodeDieciseis = document.querySelector('.episode-dieciseis');
 const close = document.querySelector('.close');
 const closeUno = document.querySelector('.close-uno')
 const closeDos = document.querySelector('.close-dos')
+const closeTres = document.querySelector('.close-tres')
+const closeCuatro = document.querySelector('.close-cuatro')
+const closeCinco = document.querySelector('.close-cinco')
+const closeSeis = document.querySelector('.close-seis')
+const closeSiete = document.querySelector('.close-siete')
+const closeOcho = document.querySelector('.close-ocho')
+const closeNueve = document.querySelector('.close-nueve')
+const closeDiez = document.querySelector('.close-diez')
+const closeOnce = document.querySelector('.close-once')
+const closeDoce = document.querySelector('.close-doce')
+const closeTrece = document.querySelector('.close-trece')
+const closeCatorce = document.querySelector('.close-catorce')
+const closeQuince = document.querySelector('.close-quince')
+const closeDieciseis = document.querySelector('.close-dieciseis')
 
 
 watch.addEventListener('click', (e)=>{
@@ -37,4 +79,144 @@ dos.addEventListener('click', (e)=>{
 closeDos.addEventListener('click', (e)=>{
     e.preventDefault();
     episodeDos.classList.remove('active');
+});
+
+tres.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeTres.classList.add('active');
+});
+
+closeTres.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeTres.classList.remove('active');
+});
+
+cuatro.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeCuatro.classList.add('active');
+});
+
+closeCuatro.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeCuatro.classList.remove('active');
+});
+
+cinco.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeCinco.classList.add('active');
+});
+
+closeCinco.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeCinco.classList.remove('active');
+});
+
+seis.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeSeis.classList.add('active');
+});
+
+closeSeis.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeSeis.classList.remove('active');
+});
+
+siete.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeSiete.classList.add('active');
+});
+
+closeSiete.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeSiete.classList.remove('active');
+});
+
+ocho.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeOcho.classList.add('active');
+});
+
+closeOcho.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeOcho.classList.remove('active');
+});
+
+nueve.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeNueve.classList.add('active');
+});
+
+closeNueve.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeNueve.classList.remove('active');
+});
+
+diez.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeDiez.classList.add('active');
+});
+
+closeDiez.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeDiez.classList.remove('active');
+});
+
+once.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeOnce.classList.add('active');
+});
+
+closeOnce.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeOnce.classList.remove('active');
+});
+
+doce.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeDoce.classList.add('active');
+});
+
+closeDoce.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeDoce.classList.remove('active');
+});
+
+trece.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeTrece.classList.add('active');
+});
+
+closeTrece.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeTrece.classList.remove('active');
+});
+
+catorce.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeCatorce.classList.add('active');
+});
+
+closeCatorce.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeCatorce.classList.remove('active');
+});
+
+quince.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeQuince.classList.add('active');
+});
+
+closeQuince.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeQuince.classList.remove('active');
+});
+
+dieciseis.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeDieciseis.classList.add('active');
+});
+
+closeDieciseis.addEventListener('click', (e)=>{
+    e.preventDefault();
+    episodeDieciseis.classList.remove('active');
 });
